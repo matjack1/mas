e97c96b5d3598c210eae7eb3943cc2e5ee366c2a
Matteo Giaccone <matteo.giaccone@welaika.com>
2014-02-24 09:17:20 +0000

    Rails and RSpec setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mas::Application.config.secret_key_base = '393ac3de533515ffc665db2355d65cc5c2c9dd1d610aa6650733cb5e8f99f4e7a78693a783a7b9946f1e7f3666a6a976802dfff2dfc001023dc0fcf7e439ea8b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Mas::Application.config.session_store :cookie_store, key: '_mas_session'
